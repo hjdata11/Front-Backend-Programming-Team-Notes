c9bdeaed97da5f9ac1da71559b05e4dc284d6da3
Hunjin Chung <35986429+hjdata11@users.noreply.github.com>
2021-10-08 16:14:56 +0900

    Add files via upload

---
+++ Instaclone-Native/apollo.js
@@ -0,0 +1,67 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { offsetLimitPagination } from "@apollo/client/utilities";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {onError} from "@apollo/client/link/error";
+import {createUploadLink} from "apollo-upload-client";
+
+export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
+
+const TOKEN = "token";
+
+export const logUserIn = async (token) => {
+  await AsyncStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
+  tokenVar(token);
+};
+
+export const logUserOut = async () => {
+  await AsyncStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+  tokenVar(null);
+};
+
+const uploadHttpLink = createUploadLink({
+  uri: "https://rude-earwig-33.loca.lt/graphql"
+  // uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
+
+const onErrorLink = onError(({graphQLErrors, networkError}) => {
+  if (graphQLErrors) {
+      console.log("GraphQL Error", graphQLErrors);
+  } else if (networkError) {
+      console.log("Network Error", networkError);
+  }
+})
+
+
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        seeFeed: offsetLimitPagination(),
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(onErrorLink).concat(uploadHttpLink),
+  cache,
+});
+export default client;
